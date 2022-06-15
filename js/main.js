794af2003688da20a3ca568fb12435f6c610daa0
SamueleLettieri <samuele2073@gmail.com>
2022-06-15 20:33:06 +0200

    risolto errore di scrittura

--- js/main.js
+++ js/main.js
@@ -22,14 +22,14 @@ sending.addEventListener("click", function(){
     } else {
         if (yourAge < 18){
             price = normalRunning - normalRunning * 15 / 100;
-            message = "Abbiamo confrontato i prezzi che si trovano 1 su siti di viaggi è questo e il costo medio a biglietto, essendo minorenne ha diritto allo sconto del 15% sul prezzo del biglietto: ";
+            message = "Abbiamo confrontato i prezzi che si trovano sui siti di viaggi è questo e il costo medio a biglietto, essendo minorenne ha diritto allo sconto del 15% sul prezzo del biglietto: ";
             document.getElementById("ticket-price").innerHTML =  message +  price.toFixed(2) + "€";
         } else if (yourAge > 65) {
             price = normalRunning - normalRunning * 35 / 100;
-            message = "Abbiamo confrontato i prezzi che si trovano 1 su siti di viaggi è questo e il costo medio a biglietto, essendo over 65 ha diritto allo sconto del 35% sul prezzo del biglietto: ";
+            message = "Abbiamo confrontato i prezzi che si trovano sui siti di viaggi è questo e il costo medio a biglietto, essendo over 65 ha diritto allo sconto del 35% sul prezzo del biglietto: ";
             document.getElementById("ticket-price").innerHTML =  message +  price.toFixed(2) + "€";
         } else {
-            message = "Abbiamo confrontato i prezzi che si trovano 1 su siti di viaggi è questo e il costo medio a biglietto: "
+            message = "Abbiamo confrontato i prezzi che si trovano sui siti di viaggi è questo e il costo medio a biglietto: "
             document.getElementById("ticket-price").innerHTML = message + normalRunning.toFixed(2) + "€";
         }
     } 
